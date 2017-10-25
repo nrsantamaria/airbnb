e4b8e1f67ab697d420fb2bd3fa7df6d5528c87d0
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-10-25 09:58:33 -0700

    need to install npm install babel-core@6.24.1 babel-loader@7.0.0 babel-preset-es2015@6.24.1 babel-preset-react@6.24.1 --save-dev

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "./Nav";
+
+function App(props){
+  return (
+    <div>
+      <Nav/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Nav(props){
+  return(
+    <h1>Soon a Nav</h1>
+  );
+}
+
+export default Nav;
