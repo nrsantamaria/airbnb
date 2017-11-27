fddfff41a691b7b48dc05c0cb627b63ab6abc8b5
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-11-26 16:43:10 -0800

    add home list component

---
+++ src/components/HomeList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from "./Home";
+
+function HomeList(props){
+  return (
+    <Home
+      roomType="Entire Home"
+      beds=2
+      name="Vanilla Villa"
+      price=230
+      rating=500/>
+  );
+}
+
+export default HomeList;
